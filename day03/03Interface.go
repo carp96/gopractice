bed4679b309b33e2eb30a960af166e381b0b0c05
carpli(黎瑜) <carpli@futunn.com>
2022-04-19 17:56:09 +0800

    一点补充

--- day03/03Interface.go
+++ day03/03Interface.go
@@ -18,25 +18,26 @@ type C interface {
 
 type str string
 
-func (s str) PrintWord() {
+func (s *str) PrintWord() {
 	fmt.Println("Hello world!")
 }
 
-func (s str) JoinStr(ss string) {
+func (s *str) JoinStr(ss string) {
 	// s += str	// 报错，str和string类型不同，有点奇怪，明明是个别名
-	s += str(ss) //强制类型转换
+	*s += str(ss) //强制类型转换
 }
 
-func (s str) SubStr() string {
-	return string(s)[2:]
+func (s *str) SubStr() string {
+	return string(*s)[2:]
 }
 
 func main() {
 	var cclass C
-	var s str
+	var s = new(str)
 	cclass = s
 	cclass.PrintWord()
 	cclass.JoinStr("hhh")
-	cclass = str("hello hei")
+	*s = str("hello hei")
+	cclass = s
 	fmt.Println(cclass.SubStr())
 }
